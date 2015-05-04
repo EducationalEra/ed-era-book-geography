266872f2d1bf0e63ad755fac48beef291f971c66
artemilchuk <artem.ilchuk@ed-era.com>
2015-05-04 14:48:15 +0300

    Update photoswipe.js

--- custom/js/photoswipe.js
+++ custom/js/photoswipe.js
@@ -2,9 +2,3 @@
     var hammertime = Hammer(element).on("tap", function(event) {
         alert('hello!');
     });
-    
-  ("#zoom_07").elevateZoom({
-  zoomType				: "lens",
-  lensShape : "round",
-  lensSize    : 200
-});
