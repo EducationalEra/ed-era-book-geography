de27f5df630acd13bb10ba6fc086f213be02b4e7
artemilchuk <artem.ilchuk@ed-era.com>
2015-07-29 14:41:28 +0300

    Update custom/js/test.js

--- custom/js/test.js
+++ custom/js/test.js
@@ -4,11 +4,6 @@ $(document).load($(window).bind(pupsik));
     {
         if ($( "div" ).hasClass( "with-summary" ))
         {
-        $("div.book-body").css({"background": "yellow"});
-          /*if($(window).width() < 820px)
-          {
-              $(".text").css({'visibility': 'hidden', 'width': '1px','height':'1px'});
-              $(".btn-knopka").css({'display': 'block', 'position': 'relative', 'width': '140px', 'height': '30px', 'text-align': 'center', 'top': '6px', 'margin': '4px auto'});
-          }*/
+        $("div.book-body").css({"color", "yellow"});
       }
     }
