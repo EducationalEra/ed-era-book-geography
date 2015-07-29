0b7d7d7295e2f27dc2181cd4cee873a31ea0b6db
artemilchuk <artem.ilchuk@ed-era.com>
2015-07-29 14:29:57 +0300

    Update custom/js/test.js

--- custom/js/test.js
+++ custom/js/test.js
@@ -2,12 +2,13 @@ $(document).load($(window).bind(pupsik));
 
     function pupsik()
     {
-        if ($( ".book" ).hasClass( "foo" ))
+        if ($( "div" ).hasClass( "with-summary" ))
         {
-          if($(window).width() < 820px)
+        $(".with-summary").css({"font-style": "italic"});
+          /*if($(window).width() < 820px)
           {
               $(".text").css({'visibility': 'hidden', 'width': '1px','height':'1px'});
               $(".btn-knopka").css({'display': 'block', 'position': 'relative', 'width': '140px', 'height': '30px', 'text-align': 'center', 'top': '6px', 'margin': '4px auto'});
-          }
+          }*/
       }
     }
