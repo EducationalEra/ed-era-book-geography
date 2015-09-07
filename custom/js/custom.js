9ec63b4c2f18ae46061ad2437946693d02e5b9cf
artemilchuk <artem.ilchuk@ed-era.com>
2015-09-07 16:46:41 +0300

    Update custom/js/custom.js

--- custom/js/custom.js
+++ custom/js/custom.js
@@ -7,3 +7,24 @@ $(document).ready(function () {
   href = "http://" + _.last(href.split("-")) + ".ed-era.com";
   $(".custom-link:last").prop("href", href);
 });
+
+$("img#map1").hide();
+$("img#map2").hide();
+$("img#map3").hide();
+$(document).on('click', '.show-map', function() {
+	if($('input#map1').prop( "checked" ))
+		$("img#map1").show();
+    if(!$('input#map1').prop( "checked" )){
+    	$("img#map1").hide();
+    }
+    if($('input#map2').prop( "checked" ))
+		$("img#map2").show();
+    if(!$('input#map2').prop( "checked" )){
+    	$("img#map2").hide();
+    }    
+    if($('input#map3').prop( "checked" ))
+		$("img#map3").show();
+    if(!$('input#map3').prop( "checked" )){
+    	$("img#map3").hide();
+    }    
+});
