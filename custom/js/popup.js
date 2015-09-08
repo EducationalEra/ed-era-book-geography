ccc3218a8b0f0f21fbaca6f1d0fcde7b3c72d8ac
artemilchuk <artem.ilchuk@ed-era.com>
2015-09-08 21:38:37 +0300

    Update custom/js/popup.js

--- custom/js/popup.js
+++ custom/js/popup.js
@@ -1,8 +1,3 @@
-$(document).ready( function(){
-   $( "div.popup" ).replaceWith( "<div class='" + "description" + "'><div class='" + "text" +"'>Ще більше відеоматеріалів в <a class='"+"knopka1"+"' href='" + "https://study.ed-era.com/courses/EdEra/G101/g101/about" + "' target='" + "_blank" + "'>онлайн-курсі</a> від розробників цієї книги</div><div class='" + "btn-knopka" +"'><a class='"+"knopka"+"' href='" + "https://study.ed-era.com/courses/EdEra/g102/G102/about" + "'   target='" + "_blank" + "'>Перейти на курс</a></div>" );
-   console.log('0');
-   });
-   
 $(document).on('click', 'li.chapter', function(){
     console.log('1');
     $(document).ready( function(){
