be32d2284b81f34074b874b836b0e28dc97d838d
Olga Filipova <chudaol@gmail.com>
2015-09-22 23:24:12 +0200

    added all custom js functions to the gitbook page change event handler.

--- custom/js/custom.js
+++ custom/js/custom.js
@@ -1,31 +1,37 @@
 $(document).ready(function () {
-  var href, $lastLink;
-  
-  $("a.toggle-search").remove();
-  
-  $lastLink = $(".custom-link:last");
-  href = $lastLink.prop("href");
-  href = "https://ed-era.com/books/" + _.last(href.split("-"));
-  $lastLink.prop("href", href);
-});
+  function init () {
+    var href, $lastLink;
 
-$("img#map1").hide();
-$("img#map2").hide();
-$("img#map3").hide();
-$(document).on('click', '.show-map', function() {
-	if($('input#map1').prop( "checked" ))
-		$("img#map1").show();
-    if(!$('input#map1').prop( "checked" )){
-    	$("img#map1").hide();
-    }
-    if($('input#map2').prop( "checked" ))
-		$("img#map2").show();
-    if(!$('input#map2').prop( "checked" )){
-    	$("img#map2").hide();
-    }    
-    if($('input#map3').prop( "checked" ))
-		$("img#map3").show();
-    if(!$('input#map3').prop( "checked" )){
-    	$("img#map3").hide();
-    }    
+    $("a.toggle-search").remove();
+
+    $lastLink = $(".custom-link:last");
+    href = $lastLink.prop("href");
+    href = "https://ed-era.com/books/" + _.last(href.split("-"));
+    $lastLink.prop("href", href);
+    $("img#map1").hide();
+    $("img#map2").hide();
+    $("img#map3").hide();
+    $(document).on('click', '.show-map', function() {
+      if($('input#map1').prop( "checked" ))
+        $("img#map1").show();
+      if(!$('input#map1').prop( "checked" )){
+        $("img#map1").hide();
+      }
+      if($('input#map2').prop( "checked" ))
+        $("img#map2").show();
+      if(!$('input#map2').prop( "checked" )){
+        $("img#map2").hide();
+      }
+      if($('input#map3').prop( "checked" ))
+        $("img#map3").show();
+      if(!$('input#map3').prop( "checked" )){
+        $("img#map3").hide();
+      }
+    });
+  }
+
+  init();
+  require(["gitbook"], function (gitbook) {
+    gitbook.events.bind("page.change", init);
+  });
 });
