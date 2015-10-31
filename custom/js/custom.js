bc89fbd3cf1d14585d72c83ced8b1fd45e255877
chudaol <chudaol@gmail.com>
2015-11-01 00:30:16 +0200

    Update custom/js/custom.js

--- custom/js/custom.js
+++ custom/js/custom.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
   function init () {
     var href, $lastLink;
-
+    
+    $("a[aria-label=Search]").remove();
     $("a.toggle-search").remove();
 
     $lastLink = $(".custom-link:last");
