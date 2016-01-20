c6298b72148cfe2a57f94bc6896d6c543da39636
artemilchuk <artem.ilchuk@ed-era.com>
2016-01-20 13:13:58 +0200

    Update custom/js/popup.js

--- custom/js/popup.js
+++ custom/js/popup.js
@@ -1,6 +1,6 @@
 $( window ).load( function(){
     function init () {
-        $( "div.popup" ).replaceWith( "<div class='" + "description" + "'><div class='" + "text" +"'>Ще більше відеоматеріалів в <a class='"+"knopka1"+"' href='" + "https://study.ed-era.com/courses/EdEra/g102/G102/about" + "' target='" + "_blank" + "'>онлайн-курсі</a> від розробників цієї книги</div><div class='" + "btn-knopka" +"'><a class='"+"knopka"+"' href='" + "https://study.ed-era.com/courses/EdEra/g102/G102/about" + "'   target='" + "_blank" + "'>Перейти на курс</a></div>" );
+        $( "div.popup" ).replaceWith( "<div class='" + "description" + "'><div class='" + "text" +"'>Ще більше відеоматеріалів в <a class='"+"knopka1"+"' href='" + "https://study.ed-era.com/courses/EdEra/g102/G102/about" + "' target='" + "_blank" + "'>онлайн-курсі</a> від розробників цієї книги</div><div class='" + "btn-knopka" +"'><a class='"+"knopka"+"' href='" + "https://study.ed-era.com/courses/EdEra/g102/G102/about" + "'   target='" + "_blank" + "'>Перейти на курс</a></div>" ).after( "<div class='space'></div>" );
     }
 
     init();
