4eabde6c398edf38cc4032b0aa438f062168018d
artemilchuk <artem.ilchuk@ed-era.com>
2017-09-25 14:56:43 +0300

    added set interval that waits for mathjax

--- custom/js/custom.js
+++ custom/js/custom.js
@@ -29,30 +29,5 @@ $(document).ready(function () {
         clearInterval(interval);
       }
     }, 1000);
-    $("img#map1").hide();
-    $("img#map2").hide();
-    $("img#map3").hide();
-    $(document).on('click', '.show-map', function() {
-      if($('input#map1').prop( "checked" ))
-        $("img#map1").show();
-      if(!$('input#map1').prop( "checked" )){
-        $("img#map1").hide();
-      }
-      if($('input#map2').prop( "checked" ))
-        $("img#map2").show();
-      if(!$('input#map2').prop( "checked" )){
-        $("img#map2").hide();
-      }
-      if($('input#map3').prop( "checked" ))
-        $("img#map3").show();
-      if(!$('input#map3').prop( "checked" )){
-        $("img#map3").hide();
-      }
-    });
-  }
-
-  init();
-  require(["gitbook"], function (gitbook) {
-    gitbook.events.bind("page.change", init);
   });
 });
